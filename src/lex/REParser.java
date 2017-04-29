17a58c60928827d2dce7f6d7bba06f8f6d512767
xuemenghu <xuemenghu.me@gmail.com>
2017-04-29 13:25:47 +0800

    regular expression parser

---
+++ src/lex/REParser.java
@@ -0,0 +1,19 @@
+package lex;
+
+public class REParser {
+    private void preprocessRE(String re) {
+        StringBuilder sb = new StringBuilder(re);
+        for (int i = 0; i < sb.length(); i++) {
+
+        }
+    }
+
+    private void infixToPostfix(String infix) {
+
+    }
+
+    public static void main(String[] args) {
+        String a = "a\\ab";
+        System.out.println(a);
+    }
+}
