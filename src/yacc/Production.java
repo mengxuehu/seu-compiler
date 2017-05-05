23121907655e4a1767678a289a7993f86400dfed
xuemenghu <xuemenghu.me@gmail.com>
2017-05-05 19:27:30 +0800

    production

--- src/yacc/Production.java
+++ src/yacc/Production.java
@@ -6,14 +6,20 @@ import java.util.List;
 class Production {
     private int head;
     private List<Integer> body;
+    private int index;
 
-    Production(int head, List<Integer> body) {
+    Production(int head, List<Integer> body, int index) {
         this.head = head;
         this.body = body;
+        this.index = index;
     }
 
     int getHead() {
         return head;
+    }
+
+    int getIndex() {
+        return index;
     }
 
     List<Integer> getBody() {
