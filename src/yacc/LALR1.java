3086346572fa949d82e17dc44e6eab6651f13642
sxk <269531918@qq.com>
2017-05-08 18:47:21 +0800

    LALR1 finished

--- src/yacc/LALR1.java
+++ src/yacc/LALR1.java
@@ -1,21 +1,122 @@
 package yacc;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+
+import javafx.util.Pair;
 
 /**
  * Created by sxk on 06/05/17.
  */
 public class LALR1 {
 
-    private Set<ItemSet> itemSetOfLR1;
     private Set<UnionItemSet> itemSetOfLALR1 = new HashSet<>();
+    private Map<Pair<Integer, Integer>, Integer> tableGoto = new HashMap<>();
+    private Map<Pair<Integer, Integer>, Action> tableAction = new HashMap<>();
+    
+    
+    public Map<Pair<Integer, Integer>, Integer> getTableGoto() {
+		return tableGoto;
+	}
 
-    LALR1(Set<ItemSet> itemSets) {
-        this.itemSetOfLR1 = itemSets;
+	public void setTableGoto(Map<Pair<Integer, Integer>, Integer> tableGoto) {
+		this.tableGoto = tableGoto;
+	}
+
+	public Map<Pair<Integer, Integer>, Action> getTableAction() {
+		return tableAction;
+	}
+
+	public void setTableAction(Map<Pair<Integer, Integer>, Action> tableAction) {
+		this.tableAction = tableAction;
+	}
+
+	
+    
+    LALR1() {
+    	
     }
+    
+    public boolean generateLALR1(Set<ItemSet> itemSetOfLR1) {
+		unionItemSet(itemSetOfLR1);
+		generateTable(itemSetOfLR1);
+		return false;
+	}
+    
+    private boolean generateTable(Set<ItemSet> itemSetOfLR1) {
+    	Map<Pair<Integer, Integer>, Integer> newTableGoto = new HashMap<>();
+        Map<Pair<Integer, Integer>, Action> newTableAction = new HashMap<>();
+        
+    	for (Pair<Integer, Integer> gotoPair : tableGoto.keySet()) {
+    		Integer gotoPairKey = 0;
+			Integer gotoPairVal = 0;
+    		for (UnionItemSet unionItemSet : itemSetOfLALR1) {
+				for (ItemSet itemSet : unionItemSet.getAllItemSet()) {
+					if (itemSet.getState() == gotoPair.getKey()) {
+						gotoPairKey = unionItemSet.getState();
+						break;
+					}
+				}
+				gotoPairVal = gotoPair.getValue();
+				newTableGoto.put(new Pair<Integer, Integer>(gotoPairKey, gotoPairVal), tableGoto.get(gotoPair));
+			}
+		}
+    	
+    	for (Pair<Integer, Integer> actionPair : tableAction.keySet()) {
+    		Integer actionPairKey = 0;
+			Integer actionPairVal = 0;
+			boolean conflict = false;
+    		for (UnionItemSet unionItemSet : itemSetOfLALR1) {
+				for (ItemSet itemSet : unionItemSet.getAllItemSet()) {
+					if (itemSet.getState() == actionPair.getKey()) {
+						actionPairVal = unionItemSet.getState();
+						break;
+					}
+				}
+				actionPairVal = actionPair.getValue();
+				Pair<Integer, Integer> action = new Pair<Integer, Integer>(actionPairKey, actionPairVal);
+				for (Pair<Integer, Integer> exist : newTableAction.keySet()) {
+					if (exist.equals(action)) {
+						//冲突处理代码
+						conflict = true;
+					}
+				}
+				if (!conflict) {
+					newTableGoto.put(action, tableGoto.get(actionPair));
+				} else {
+					//冲突解决添加或修改
+				}
+				
+			}
+		}
+    	
+		return false;
+	}
 
+    private void unionItemSet(Set<ItemSet> itemSetOfLR1) {
+
+        //合并项集
+        boolean ifAdd = false;
+        int state = 0;
+        for (ItemSet itemSet : itemSetOfLR1) {
+            ifAdd = false;
+            for (UnionItemSet unionItemSet : itemSetOfLALR1) {
+                if (itemSet.equalItemSet(unionItemSet.getFirstItem())) {
+                    unionItemSet.addItemSet(itemSet);
+                    ifAdd = true;
+                }
+            }
+            if (!ifAdd) {
+                UnionItemSet unionSet = new UnionItemSet();
+                unionSet.addItemSet(itemSet);
+                itemSetOfLALR1.add(unionSet);
+            }
+        }
+    }
+        
     class UnionItemSet {
         private int state;
         private ArrayList<ItemSet> unionItemSet = new ArrayList<>();
@@ -24,6 +125,10 @@ public class LALR1 {
         public ItemSet getFirstItem() {
             return unionItemSet.get(0);
         }
+        
+        public ArrayList<ItemSet> getAllItemSet() {
+			return unionItemSet;
+		}
 
         public ArrayList<Set<Integer>> getLookaheadSymbols() {
             return lookaheadSymbols;
@@ -65,30 +170,5 @@ public class LALR1 {
             this.state = state;
         }
     }
-
-    public void generateLALR1() {
-
-        //合并项集
-        boolean ifAdd = false;
-        int state = 0;
-        for (ItemSet itemSet : itemSetOfLR1) {
-            ifAdd = false;
-            for (UnionItemSet unionItemSet : itemSetOfLALR1) {
-                if (itemSet.equalItemSet(unionItemSet.getFirstItem())) {
-                    unionItemSet.addItemSet(itemSet);
-                    ifAdd = true;
-                }
-            }
-            if (!ifAdd) {
-                UnionItemSet unionSet = new UnionItemSet();
-                unionSet.addItemSet(itemSet);
-                itemSetOfLALR1.add(unionSet);
-            }
-        }
-
-        //添加向前看符号
-        for (UnionItemSet unionItemSet : itemSetOfLALR1) {
-
-        }
-    }
+    
 }
