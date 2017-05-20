11790b3fe0e3e226eb46e86de83831a5e1f925c2
xuemenghu <xuemenghu.me@gmail.com>
2017-05-20 15:48:07 +0800

    yacc top entry

--- src/yacc/Yacc.java
+++ src/yacc/Yacc.java
@@ -1,7 +1,29 @@
 package yacc;
 
-/**
- * Created by hu on 5/19/17.
- */
+
+import javafx.util.Pair;
+
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Set;
+
 public class Yacc {
+    Yacc(String source) {
+        YaccSourceParser yaccSourceParser = new YaccSourceParser();
+        yaccSourceParser.parse(source);
+        String programs = yaccSourceParser.getPrograms();
+        Productions productions = yaccSourceParser.getProductions();
+        Symbols symbols = yaccSourceParser.getSymbols();
+
+        LR1 lr1 = new LR1();
+        lr1.parse(productions, symbols);
+        Map<Pair<Integer, Integer>, Integer> tableGoto = lr1.getTableGoto();
+        Map<Pair<Integer, Integer>, Action> tableAction = lr1.getTableAction();
+        Set<ItemSet> collection = lr1.getCollection();
+    }
+
+    public static void main(String[] args) {
+        String sourcePath = Paths.get(System.getProperty("user.dir"), "c99.y").toString();
+        new Yacc(sourcePath);
+    }
 }
