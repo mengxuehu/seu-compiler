c95992e0307207026b6534fe2797fb7430c27a4f
xuemenghu <xuemenghu.me@gmail.com>
2017-05-22 19:45:41 +0800

    parser 1

--- src/yacc/ParserSourceGenerator.java
+++ src/yacc/ParserSourceGenerator.java
@@ -3,13 +3,10 @@ package yacc;
 import javafx.util.Pair;
 import yacc.util.YaccStringBuilder;
 
-import java.util.ArrayList;
 import java.util.Map;
 
 class ParserSourceGenerator {
     private static final String HEADER_NAME = "yacc.tab.h", SOURCE_NAME = "yacc.tab.cpp";
-    ParserSourceGenerator() {
-    }
 
     void generate(Map<Pair<Integer, Integer>, Integer> tableGoto,
                   Map<Pair<Integer, Integer>, Action> tableAction,
@@ -22,34 +19,157 @@ class ParserSourceGenerator {
                                 Map<Pair<Integer, Integer>, Action> tableAction,
                                 Productions productions, String programs) {
         YaccStringBuilder source = new YaccStringBuilder();
-        source.appendLine(programs);
+
+        /* includes */
+        source.appendLine("#include <map>")
+                .appendLine("#include <stack>")
+                .appendLine("#include <utility>")
+                .appendLine("#include <iostream>")
+                .appendBlankLine();
+
+        /* user programs */
+        source.appendLine(programs).appendBlankLine();
+
+        /* parsing table */
+        generateData(tableGoto, tableAction, productions, source);
+
+        /* main, ... */
         source.appendLine("void yyparse(void);");
+        source.setNumTabs(1);
         source.appendLine("int main() {")
-                .appendLineWithTabs("yyparse();", 1)
-                .appendLineWithTabs("return 0;", 1)
+                .appendLineWithTabs("yyparse();")
+                .appendLineWithTabs("return 0;")
                 .appendLine("}");
-        YaccStringBuilder front = new YaccStringBuilder(), back = new YaccStringBuilder();
-        front.appendLine("void yyparse(void) {");
-        back.insertFrontLine("}");
+        source.decreaseNumTabs();
 
-        int numTabs = 1;
+        /* yyparse() */
+        source.appendLine("void yyparse(void) {");
 
+        source.increaseNumTabs();
 
+        source.appendLineWithTabs("stack<int> stack_state, stack_symbol;");
+        source.appendWithTabs("stack_state.push(")
+                .append(productions.getStart().getIndex().toString()).appendLine(");");
+
+        // while begin
+        source.appendLineWithTabs("while (true) {");
+        source.increaseNumTabs();
+
+        source.appendLineWithTabs("int next = yylex();");
+        source.appendLineWithTabs("std::pair<int, int> key(stack_state.top(), next);");
+        source.appendLineWithTabs("std::pair<int, int> action;");
+        source.appendLineWithTabs("auto it = table_action.find(key);");
+
+        // if begin
+        source.appendLineWithTabs("if ( it == table_action.end() {");
+        source.appendLineWithTabs("\tstd::cerr << \"Error: unknown token << std::endl;");
+        source.appendLineWithTabs("} else {");
+        source.appendLineWithTabs("\taction = std::make_pair(it -> first, it -> second);");
+        source.appendLineWithTabs("}");
+        // if end
+
+        // switch begin
+        String switchString = "switch (action.first) {\n" +
+                "\tcase SHIFT:\n" +
+                "\t\tstack_state.push(action.second);\n" +
+                "\t\tstack_symbol.push(next);\n" +
+                "\t\tnext = yylex();\n" +
+                "\t\tbreak;\n" +
+                "\tcase REDUCE:\n" +
+                "\t\tint head = productions[action.second].first;\n" +
+                "\t\tint body_size = productions[action.second].second;\n" +
+                "\t\tfor (int i = 0; i < body_size; ++i) {\n" +
+                "\t\t\tstack_state.pop();\n" +
+                "\t\t\tstack_symbol.pop();\n" +
+                "\t\t}\n" +
+                "\t\tkey = std::make_pair(stack_stack.top(), head);\n" +
+                "\t\tauto iter = table_goto.find(key);\n" +
+                "\t\tif (key == table_goto.end()) {\n" +
+                "\t\t\tstd::cerr << \"Error: Unknown token\" << std::endl;\n" +
+                "\t\t} else {\n" +
+                "\t\t\tstack_state.push(*iter);\n" +
+                "\t\t\tstack_symbol.push(head);\n" +
+                "\t\t}\n" +
+                "\t\tbreak;\n" +
+                "\tcase ACCEPT:\n" +
+                "\t\treturn;\n" +
+                "\t\tbreak;\n" +
+                "\tdefault:\n" +
+                "\t\tstd::cerr << \"Error: Unknown action type\" << std::endl;\n" +
+                "}";
+
+        while (source.getNumTabs() != 0) {
+            source.decreaseNumTabs();
+            source.appendLineWithTabs("}");
+        }
+        // all end
         System.out.println(source.toString());
-        System.out.println(front.toString());
-        System.out.println(back.toString());
+    }
+
+    private void generateData(Map<Pair<Integer, Integer>, Integer> tableGoto,
+                              Map<Pair<Integer, Integer>, Action> tableAction,
+                              Productions productions,
+                              YaccStringBuilder source) {
+        // goto
+        source.appendLine("std::map<std::pair<int, int>, int> table_goto = {");
+        for (Map.Entry<Pair<Integer, Integer>, Integer> entry : tableGoto.entrySet()) {
+            source.append("\t{{")
+                    .append(entry.getKey().getKey().toString()).append(", ")
+                    .append(entry.getKey().getValue().toString()).append("}, ")
+                    .append(entry.getValue().toString()).appendLine("},");
+        }
+        source.appendLine("};").appendBlankLine();
+
+        // action
+        source.appendLine("const int ACCEPT = 0, SHIFT = 1, REDUCE = 2;");
+        final int ACCEPT = 0, SHIFT = 1, REDUCE = 2;
+        source.appendLine("std::map<std::pair<int, int>, std::pair<int, int>> table_action = {");
+        for (Map.Entry<Pair<Integer, Integer>, Action> entry : tableAction.entrySet()) {
+            Integer type = -1, target = -1;
+            switch (entry.getValue().getType()) {
+                case ACC:
+                    type = ACCEPT;
+                    target = -1;
+                    break;
+                case SHIFT:
+                    type = SHIFT;
+                    target = ((ShiftAction)entry.getValue()).getShiftTarget();
+                    break;
+                case REDUCE:
+                    type = REDUCE;
+                    target = ((ReduceAction)entry.getValue()).getProductionReducingBy();
+            }
+            source.append("\t{{")
+                    .append(entry.getKey().getKey().toString()).append(", ")
+                    .append(entry.getKey().getValue().toString()).append("}, {")
+                    .append(type.toString()).append(", ")
+                    .append(target.toString()).appendLine("}},");
+        }
+        source.appendLine("};").appendBlankLine();
+
+        // productions
+        source.appendLine("std::map<int, std::pair<int, int>> productions = {");
+        for (Production production : productions.getProductions()) {
+            source.append("\t{").append(production.getIndex().toString())
+                    .append(", {").append(production.getHead().toString())
+                    .append(", ").append(String.valueOf(production.getBody().size()))
+                    .appendLine("}}");
+        }
+        source.appendLine("};").appendBlankLine();
+
+        source.append("start_production = ").append(productions.getStart().toString())
+                .appendLine(";").appendBlankLine();
     }
 
     private void generateHeader(Symbols symbols) {
         YaccStringBuilder header = new YaccStringBuilder();
-//        header.appendLine("enum yytokentype {");
         for (Map.Entry<String, Integer> sym : symbols.getSymbols().entrySet()) {
             if (symbols.isTerminal(sym.getValue())
                     && (sym.getKey().length() != 1 || Character.isLetter(sym.getKey().charAt(0)))) {
                 header.append("#define ").append(sym.getKey()).append(" ").appendLine(sym.getValue().toString());
-//                header.append("\t").append(sym.getKey()).append(" = ").append(sym.getValue().toString())
-//                        .appendLine(",");
             }
         }
+
+        System.out.println(header.toString());
     }
 }
